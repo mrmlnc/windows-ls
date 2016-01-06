71d26ec33ad60f159a92a3dc3d24d76846d757db
Denis Malinochkin <dmalinochkin@rambler.ru>
2016-01-06 21:16:46 +0300

    Use `has-glob` for testing args on glob pattern

--- cli.js
+++ cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 var path = require('path');
 var glob = require('glob');
+var hasGlob = require('has-glob');
 var Fsys = require('./lib/fsys');
 var argv = require('minimist')(process.argv.slice(2));
 var dir = process.cwd();
@@ -22,10 +23,9 @@ function setOptions(argv) {
 
   // If there are arguments and the first argument with star
   if (argv._[0] && argv._[0].indexOf('-')) {
-    // If the argument is a directory
     op.dir = path.join(dir, argv._[0]);
     // If the argument contains the glob-pattern
-    op.dir = (argv._[0].indexOf('*') + 1) ? op.dir : path.join(op.dir, '*');
+    op.dir = (hasGlob(argv._[0])) ? op.dir : path.join(op.dir, '*');
   } else {
     // Default glob-pattern
     op.dir = path.join(dir, '*');
